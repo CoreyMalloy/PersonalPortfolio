49c4b0d37e1d5b6aa2d5457edab57da051245030
CoreyMalloy <Cordog_8381@outlook.com>
2024-06-04 21:44:40 -0400

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,5 @@
 import './App.css'
 
-function Greeting() {
-  return (
-    <h1 className="greeting">
-      Hello. Welcome!
-    </h1>
-  );
-}
-
-export default function App() {
-  return <Greeting/>;
-}
-
 function Goodbye() {
   return (
     <div className="goodbye">
