3ea1849d04341dc41435a83bbf56435cfd28f9ec
CoreyMalloy <Cordog_8381@outlook.com>
2024-06-04 21:54:57 -0400

    init

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,9 @@ import './App.css'
 const Goodbye = () => {
   return (
     <div className='App'>
-      {/*<h1>Bye. See Ya!</h1>*/}
-      <Goodbye />
+      <h1>Bye. See Ya!</h1>
     </div>
   );
 }
 
-export default APP;
+export default App;
