22f7cfee2839036de304bd92d991025ce6b7e27a
CoreyMalloy <Cordog_8381@outlook.com>
2024-06-11 20:56:14 -0400

    init

--- src/App.jsx
+++ src/App.jsx
@@ -16,12 +16,8 @@ function App() {
           </div>
         </div>
         <div className='bod'>
-          <div>
-            <h1>Corey Malloy</h1>
-          </div>
-          <div>
-            <p>Web Developer</p>
-          </div>
+          <h1>Corey Malloy</h1>
+          <p>Web Developer</p>
         </div>
       </div>
     </>
