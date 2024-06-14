6c630f1f86bdd3ec16d1ca0f5a485e0a480897e2
CoreyMalloy <Cordog_8381@outlook.com>
2024-06-13 21:15:12 -0400

    init

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Home';
 import About from './About';
+import Projects from './Projects';
 import Contact from './Contact';
 
 function App() {
