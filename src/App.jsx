4b52811c7a3184b2adaf097f169a84478e0c94bf
CoreyMalloy <Cordog_8381@outlook.com>
2024-06-13 20:59:55 -0400

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,19 @@
-import './App.css'
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
+/*import './App.css'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -33,4 +48,4 @@ function App() {
   )
 }
 
-export default App
+export default App*/
