c84d72d36eab111a83d6c3de5462b9e1aa858f40
CoreyMalloy <Cordog_8381@outlook.com>
2024-06-17 10:41:14 -0400

    init

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,23 +2,31 @@ import React, { useState } from 'react';
 import './styles/Contact.css';
 
 function Contact() {
-  const [val, setVal] = useState("Hello There")
+  
   const click = () => {
     alert(val)
   }
-
-  const change = event => {
-    setVal(event.target.value)
-  }
-
+   
   return (
     <>
-      <div>
-        <h1 className='head'>Contact</h1>
-        <input onChange={change}
-        value = {val}/>
-        <button onClick = {click}>Click Me</button>
-      </div>
+      <div className='container'>
+        <div className='header'>
+          <h1>Contact Me</h1>
+        </div>
+        <div>
+          <label htmlFor="name"></label>
+          <input className='name' type='text' name='name' id='name' placeholder='Name'></input>
+        </div>
+        <div>
+          <label htmlFor="email"></label>
+          <input className='email' type='email' name='email' id='email' placeholder='Email'></input>
+        </div>
+        <div>
+          <label htmlFor="message"></label>
+          <input className='message' type='text' name='message' id='message' rows='5' placeholder='Message'></input>
+        </div>
+        <button onClick={click}>Submit</button>
+      </div> 
     </>
   )
 }
