e25760ebb129bf0980351fea51d019cdbdf90239
CoreyMalloy <Cordog_8381@outlook.com>
2024-09-07 21:27:57 -0400

    init

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -24,111 +24,112 @@ import logoimg17 from './images/tail.png';
   return (
     <>
      <div>
-       <div className='intro'>
-          <h1 className='head'>About Me</h1>
-       </div>
-       <div className='bio'>
-        I am an aspiring web developer and recent graduate from Cleveland State University with a degree in Computer science and a minor in Mathematics.
-        I have previous professional experience in web development using technologies such as Angular, NestJS, and Firebase.
-        During this internship I was able to work on both the front end, where we helped modernize a current deployment, and the back end, where we worked to optimize load speeds and database queries.
-        While my professional experience is focused towards web development, my academic journey equipped me with a multitude of skills within software development.
-        Recently I have been trying to expand my horizon by developing CRUD applications using python and SQL.
-       </div>
-        <div className='skills'>Skills</div>
-       <div className='exp'>
-       
-        <div className='left'>
-          <div className='title-left'>Languages</div>
-          <div className='card-left'>
-            <ul>
-              <li className="skill-icon">
-                <img className='html' src={logoimg2}/>
-              </li>
-              <li className="skill-icon">
-                <img className='html' src={logoimg13}/>
-              </li>
-              <li className="skill-icon">
-                <img className='html' src={logoimg15}/>
-              </li>
-            </ul>
-            <ul>
-              <li className="skill-icon">
-                <img className='html' src={logoimg3}/>
-              </li>
-              <li className="skill-icon">
-                <img className='html' src={logoimg12}/>
-              </li>
-            </ul>
-            <ul>
-              <li className="skill-icon">
-                <img className='html' src={logoimg}/>
-              </li>
-              <li className="skill-icon">
-              <img className='html' src={logoimg14}/>
-              </li>
-            </ul>
-          </div>
+      <h6 className='head'>About Me</h6>
+      <div className='bio'>
+        <div className='bio-description'>
+          I am an aspiring web developer with a degree in Computer science and a minor in Mathematics from Cleveland State University.
+          I have previous professional experience in web development using technologies such as Angular, NestJS, and Firebase.
+          My academic journey equipped me with a multitude of skills within software development and not just web development.
+          Recently I have been trying to expand my horizon into other aspects of development.
         </div>
-        <div className='right'>
-          <div className='title-right'>Services</div>
-          <div className='card-right'>
-          <ul>
-              <li className="skill-icon">
-                <img className='html' src={logoimg6}/>
-              </li>
-              <li className="skill-icon">
-                <img className='html' src={logoimg8}/>
-              </li>
-            </ul>
-            <ul>
-              <li className="skill-icon">
-                <img className='html' src={logoimg11}/>
-              </li>
-              <li className="skill-icon">
-                <img className='html' src={logoimg10}/>
-              </li>
-            </ul>
-            <ul>
-              <li className="skill-icon">
-                <img className='html' src={logoimg9}/>
-              </li>
-              <li className="skill-icon">
-              </li>
-            </ul>
-          </div> 
-        </div>
-        
-       </div>
-       <div className='cont'>
-        <div className='lang'>
-              <div className='title'>Libraries/Frameworks</div>
-              <div className='list'>
+      </div>
+      <div className='third-section-wrapper'>
+          <div className='skills'>Skills</div>
+          <div className='exp'>
+          
+            <div className='left'>
+              <div className='title-left'>Languages</div>
+              <div className='card-left'>
                 <ul>
                   <li className="skill-icon">
-                    <img className='html' src={logoimg4}/>
+                    <img className='html' src={logoimg2}/>
                   </li>
                   <li className="skill-icon">
-                    <img className='html' src={logoimg5}/>
+                    <img className='html' src={logoimg13}/>
+                  </li>
+                  <li className="skill-icon">
+                    <img className='html' src={logoimg15}/>
                   </li>
                 </ul>
                 <ul>
                   <li className="skill-icon">
-                    <img className='html' src={logoimg7}/>
+                    <img className='html' src={logoimg3}/>
                   </li>
                   <li className="skill-icon">
-                    <img className='html' src={logoimg16}/>
+                    <img className='html' src={logoimg12}/>
                   </li>
                 </ul>
                 <ul>
                   <li className="skill-icon">
-                    <img className='html' src={logoimg17}/>
+                    <img className='html' src={logoimg}/>
                   </li>
                   <li className="skill-icon">
+                  <img className='html' src={logoimg14}/>
                   </li>
                 </ul>
               </div>
             </div>
-        </div>
+            <div className='right'>
+              <div className='title-right'>Services</div>
+              <div className='card-right'>
+              <ul>
+                  <li className="skill-icon">
+                    <img className='html' src={logoimg6}/>
+                  </li>
+                  <li className="skill-icon">
+                    <img className='html' src={logoimg8}/>
+                  </li>
+                </ul>
+                <ul>
+                  <li className="skill-icon">
+                    <img className='html' src={logoimg11}/>
+                  </li>
+                  <li className="skill-icon">
+                    <img className='html' src={logoimg10}/>
+                  </li>
+                </ul>
+                <ul>
+                  <li className="skill-icon">
+                    <img className='html' src={logoimg9}/>
+                  </li>
+                  <li className="skill-icon">
+                  </li>
+                </ul>
+              </div> 
+            </div>
+            
+          </div>
+          <div className='cont'>
+            <div className='lang'>
+                  <div className='title'>Libraries/Frameworks</div>
+                  <div className='list'>
+                    <ul>
+                      <li className="skill-icon">
+                        <img className='html' src={logoimg4}/>
+                      </li>
+                      <li className="skill-icon">
+                        <img className='html' src={logoimg5}/>
+                      </li>
+                    </ul>
+                    <ul>
+                      <li className="skill-icon">
+                        <img className='html' src={logoimg7}/>
+                      </li>
+                      <li className="skill-icon">
+                        <img className='html' src={logoimg16}/>
+                      </li>
+                    </ul>
+                    <ul>
+                      <li className="skill-icon">
+                        <img className='html' src={logoimg17}/>
+                      </li>
+                      <li className="skill-icon">
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+            </div>
+          </div>
      </div>
    </>
   )
