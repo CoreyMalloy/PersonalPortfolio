c122f69911dd3ae4c50d1aaf57d138478af57e07
CoreyMalloy <Cordog_8381@outlook.com>
2024-09-28 19:56:12 -0400

    init

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -11,14 +11,14 @@ const Cards = ({ services }) => {
           <div className = "skills-container-individual-cards">
             <h3 className="individual-card-titles">{item.title}</h3>
             <div className="skills-icon-list-top">
-              {item.icon ? (<img src={item.icon} alt={`${item.title} icon`} style={{ width: 'auto', height: '5rem' }} />) : null}
-              {item.icon1 ? (<img src={item.icon1} alt={`${item.title} icon1`} style={{ width: 'auto', height: '5rem' }} />) : null}
-              {item.icon2 ? (<img src={item.icon2} alt={`${item.title} icon2`} style={{ width: 'auto', height: '5rem' }} />) : null}
+              {item.icon ? (<img src={item.icon} alt={`${item.title} icon`}/>) : null}
+              {item.icon1 ? (<img src={item.icon1} alt={`${item.title} icon1`}/>) : null}
+              {item.icon2 ? (<img src={item.icon2} alt={`${item.title} icon2`}/>) : null}
             </div>
             <div className="skills-icon-list-bottom">
-              {item.icon3 ? (<img src={item.icon3} alt={`${item.title} icon3`} style={{ width: 'auto', height: '5rem' }} />) : null}
-              {item.icon4 ? (<img src={item.icon4} alt={`${item.title} icon4`} style={{ width: 'auto', height: '5rem' }} />) : null}
-              {item.icon5 ? (<img src={item.icon5} alt={`${item.title} icon5`} style={{ width: 'auto', height: '5rem' }} />) : null}
+              {item.icon3 ? (<img src={item.icon3} alt={`${item.title} icon3`}/>) : null}
+              {item.icon4 ? (<img src={item.icon4} alt={`${item.title} icon4`}/>) : null}
+              {item.icon5 ? (<img src={item.icon5} alt={`${item.title} icon5`}/>) : null}
               {item.icon6 ? (<img src={item.icon6} alt={`${item.title} icon6`} style={{ width: 'auto', height: '5rem' }} />) : null}
             </div>
           </div>
