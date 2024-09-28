1db051b647f360778c00a78d77ab2b7dc8134707
CoreyMalloy <Cordog_8381@outlook.com>
2024-09-28 19:45:52 -0400

    init

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -4,22 +4,18 @@ import './styles/Services.css';
 const Cards = ({ services }) => {
   const { subheading, service } = services[0];
   return (
-    <div className = "skills-container" style={{display:'flex', flexDirection: 'column', height: '100vh', backgroundColor: '#00ABE4'}}>
-      <h2 className = "skills-container-title" style={{display: 'flex', justifyContent: 'center', fontSize: '5rem', fontWeight: '400', color: '#E9F1FA', width: '100%'}}>{subheading}</h2>
-      <div className = "skills-container-total-cards" style={{display:'flex', justifyContent: 'space-evenly'}}>
+    <div className = "skills-container">
+      <h2 className = "skills-container-title">{subheading}</h2>
+      <div className = "skills-container-total-cards">
         {service.map((item) => (
-          <div className = "skills-container-individual-cards" style={{ margin: 'auto', display: 'flex', flexDirection:'column',
-            justifyContent: 'space-evenly', alignItems: 'center',
-            width: '30%', height: '60vh',
-            backgroundColor: '#E9F1FA', borderRadius: '5rem'
-          }}>
-            <h3 className="individual-card-titles" style={{fontSize: '3rem', fontWeight: '200', borderBottom: '2px solid #00ABE4'}}>{item.title}</h3>
-            <div className="skills-icon-list-top" style={{display: 'flex', justifyContent: 'space-evenly', alignItems: 'center', width: '100%'}}>
+          <div className = "skills-container-individual-cards">
+            <h3 className="individual-card-titles">{item.title}</h3>
+            <div className="skills-icon-list-top">
               {item.icon ? (<img src={item.icon} alt={`${item.title} icon`} style={{ width: 'auto', height: '5rem' }} />) : null}
               {item.icon1 ? (<img src={item.icon1} alt={`${item.title} icon1`} style={{ width: 'auto', height: '5rem' }} />) : null}
               {item.icon2 ? (<img src={item.icon2} alt={`${item.title} icon2`} style={{ width: 'auto', height: '5rem' }} />) : null}
             </div>
-            <div className="skills-icon-list-bottom" style={{display: 'flex', justifyContent: 'space-evenly', alignItems: 'center', width: '100%'}}>
+            <div className="skills-icon-list-bottom">
               {item.icon3 ? (<img src={item.icon3} alt={`${item.title} icon3`} style={{ width: 'auto', height: '5rem' }} />) : null}
               {item.icon4 ? (<img src={item.icon4} alt={`${item.title} icon4`} style={{ width: 'auto', height: '5rem' }} />) : null}
               {item.icon5 ? (<img src={item.icon5} alt={`${item.title} icon5`} style={{ width: 'auto', height: '5rem' }} />) : null}
