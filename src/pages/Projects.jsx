2c60f75d044e3c18951218e075740d81e5885c55
CoreyMalloy <Cordog_8381@outlook.com>
2024-09-30 17:15:53 -0400

    init

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -13,28 +13,20 @@ import discord from './images/discord.jpg';
           <h1 className='project-head'>Projects</h1>
       </div>
     </div>
-       <div className='proj-list'>
-          <ul>
-            <li className="proj">
-              <button><a href='https://github.com/CoreyMalloy/tradingBot'><img className='trading' src={trading}/></a></button>
-              <h2>AI-Trading Bot</h2>
-            </li>
-            <li className="proj">
-              <button><a href='https://github.com/CoreyMalloy/Bot'><img className='discord' src={discord}/></a></button>
-              <h2>Discord Bot</h2>
-            </li>
-          </ul>
-          <ul>
-            <li className="proj">
-              <button><a href='https://github.com/CoreyMalloy/CRUD2'><img className='crud' src={crud}/></a></button>
-              <h2>React/Python CRUD application</h2>
-            </li>
-            <li className="proj">
-              
-            </li>
-          </ul>
-       </div>
-     
+    <div className='proj-list'>
+      <li className="proj">
+        <button><a href='https://github.com/CoreyMalloy/tradingBot'><img className='trading' src={trading}/></a></button>
+        <h2>AI-Trading Bot</h2>
+      </li>
+      <li className="proj">
+        <button><a href='https://github.com/CoreyMalloy/Bot'><img className='discord' src={discord}/></a></button>
+        <h2>Discord Bot</h2>
+      </li>
+      <li className="proj">
+        <button><a href='https://github.com/CoreyMalloy/CRUD2'><img className='crud' src={crud}/></a></button>
+        <h2>React/Python CRUD application</h2>
+        </li>
+    </div>
    </>
   )
  }
